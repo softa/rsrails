f40bb65a3e8111a201616a715d231a60fa3b4804
Pedro Axelrud <pedro@softa.com.br>
2011-02-21 17:11:48 -0300

    migrando pra rails 2.3.11

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,4 @@
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 Rails::Initializer.run do |config|
   config.gem 'compass', :version => '>= 0.8.17'
